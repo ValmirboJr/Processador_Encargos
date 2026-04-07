23008ebe4ed22c5b99e3e6f8d15df0f23b4302e9
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:58:25 -0300

    feat: configura integração com kafka

--- src/main/java/org/example/processador_encargos/config/KafkaConfig.java
+++ src/main/java/org/example/processador_encargos/config/KafkaConfig.java
@@ -1,4 +1,35 @@
 package org.example.processador_encargos.config;
 
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic requestStatusTopic() {
+        return TopicBuilder.name("request-status-conta")
+                .partitions(32)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic responseStatusTopic() {
+        return TopicBuilder.name("response-status-conta")
+                .partitions(32)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic lancamentoContabilTopic() {
+        return TopicBuilder.name("lancamento-contabil")
+                .partitions(32)
+                .replicas(1)
+                .build();
+    }
 }
