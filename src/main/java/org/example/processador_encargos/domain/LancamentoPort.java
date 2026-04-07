71dd2a7910f9045e1ce4b40822f1dd3fa4750ac3
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:59:11 -0300

    feat: define contrato de persistência de lançamentos

--- src/main/java/org/example/processador_encargos/domain/LancamentoPort.java
+++ src/main/java/org/example/processador_encargos/domain/LancamentoPort.java
@@ -1,7 +1,9 @@
 package org.example.processador_encargos.domain;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface LancamentoRepository {
+public interface LancamentoPort {
     void salvarTodos(List<? extends Lancamento> lancamentos);
+    Lancamento buscarPorId(UUID id);
 }
