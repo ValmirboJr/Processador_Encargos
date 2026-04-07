4d7b7ab1954b7f6afc4e61cb9c3c10442519a021
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:57:49 -0300

    feat: define casos de uso de lançamentos

--- src/main/java/org/example/processador_encargos/application/usecases/LancamentoUseCases.java
+++ src/main/java/org/example/processador_encargos/application/usecases/LancamentoUseCases.java
@@ -1,4 +1,12 @@
 package org.example.processador_encargos.application.usecases;
 
-public class LancamentoUseCases {
+import org.example.processador_encargos.domain.Lancamento;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface LancamentoUseCases {
+
+    void processarLote(List<Lancamento> lancamentos);
+    Lancamento buscarPorId(UUID id);
 }
