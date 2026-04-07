68e25596a034334658bd4b0af07c83beff33820a
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:53:45 -0300

    feat: adiciona endpoint para execução de job

--- src/main/java/org/example/processador_encargos/adapters/inbound/controller/JobController.java
+++ src/main/java/org/example/processador_encargos/adapters/inbound/controller/JobController.java
@@ -1,4 +1,30 @@
 package org.example.processador_encargos.adapters.inbound.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
+@RequiredArgsConstructor
 public class JobController {
+
+    private final JobLauncher jobLauncher;
+    private final Job processarEncargosJob;
+
+    @PostMapping("/processar")
+    public ResponseEntity<String> processar() throws Exception {
+        JobParameters params = new JobParametersBuilder()
+                .addLong("timestamp", System.currentTimeMillis())
+                .toJobParameters();
+
+        jobLauncher.run(processarEncargosJob, params);
+        return ResponseEntity.ok("Iniciando Job");
+    }
 }
