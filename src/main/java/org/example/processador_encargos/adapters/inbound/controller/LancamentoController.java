708a81f01f0682bdbe5a73465d8d4dc99b91eea2
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:54:12 -0300

    feat: adiciona endpoint de consulta de lançamentos

--- src/main/java/org/example/processador_encargos/adapters/inbound/controller/LancamentoController.java
+++ src/main/java/org/example/processador_encargos/adapters/inbound/controller/LancamentoController.java
@@ -1,4 +1,30 @@
-package org.example.processador_encargos.adapters.inbound;
+package org.example.processador_encargos.adapters.inbound.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.processador_encargos.application.usecases.LancamentoUseCases;
+import org.example.processador_encargos.domain.Lancamento;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/lancamentos")
+@RequiredArgsConstructor
 public class LancamentoController {
+
+    private final LancamentoUseCases lancamentoUseCases;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Lancamento> buscarPorId(@PathVariable UUID id) {
+        Lancamento lancamento = lancamentoUseCases.buscarPorId(id);
+        if (lancamento == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(lancamento);
+    }
 }
