b0bb9f7463b4b4d5ffccc35a7b637cea33616e7b
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:46:46 -0300

    feat: ajuste de leitura de lançamentos no batch

--- src/main/java/org/example/processador_encargos/adapters/inbound/batch/LancamentoReader.java
+++ src/main/java/org/example/processador_encargos/adapters/inbound/batch/LancamentoReader.java
@@ -1,4 +1,42 @@
 package org.example.processador_encargos.adapters.inbound.batch;
 
-public class LancamentoReader {
+import org.example.processador_encargos.domain.Lancamento;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Component;
+
+@Component
+@StepScope
+public class LancamentoReader implements ItemReader<Lancamento> {
+
+    private FlatFileItemReader<Lancamento> delegate;
+
+    @BeforeStep
+    public void init(StepExecution stepExecution) {
+        ExecutionContext ctx = stepExecution.getExecutionContext();
+        String filePath = ctx.getString("filePath");
+
+        this.delegate = new FlatFileItemReaderBuilder<Lancamento>()
+                .name("lancamentoReader")
+                .resource(new FileSystemResource(filePath))
+                .linesToSkip(0)
+                .delimited()
+                .delimiter(";")
+                .names("id", "contaId", "valor", "encargos")
+                .targetType(Lancamento.class)
+                .build();
+
+        this.delegate.open(ctx);
+    }
+
+    @Override
+    public Lancamento read() throws Exception {
+        return delegate.read();
+    }
 }
