6dfe51ca8bdc617770d07d0b5c160c2b7fc12e1c
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:51:10 -0300

    feat: ajuste na escrita de lançamentos no batch

--- src/main/java/org/example/processador_encargos/adapters/inbound/batch/LancamentoWriter.java
+++ src/main/java/org/example/processador_encargos/adapters/inbound/batch/LancamentoWriter.java
@@ -1,4 +1,25 @@
 package org.example.processador_encargos.adapters.inbound.batch;
 
-public class LancamentoWriter {
+import org.example.processador_encargos.domain.Lancamento;
+import org.example.processador_encargos.domain.LancamentoPort;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@StepScope
+public class LancamentoWriter implements ItemWriter<Lancamento> {
+
+    private final LancamentoPort lancamentoPort ;
+
+    public LancamentoWriter(LancamentoPort lancamentoPort) {
+        this.lancamentoPort = lancamentoPort;
+    }
+
+    @Override
+    public void write(Chunk<? extends Lancamento> chunk) {
+        lancamentoPort.salvarTodos(chunk.getItems());
+    }
 }
