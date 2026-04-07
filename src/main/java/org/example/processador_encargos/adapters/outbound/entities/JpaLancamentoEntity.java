4c0b102b96628e3db170cb0146cb32eb584c6559
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:54:50 -0300

    feat: implementa entidade JPA de lançamentos

--- src/main/java/org/example/processador_encargos/adapters/outbound/entities/JpaLancamentoEntity.java
+++ src/main/java/org/example/processador_encargos/adapters/outbound/entities/JpaLancamentoEntity.java
@@ -1,4 +1,38 @@
 package org.example.processador_encargos.adapters.outbound.entities;
 
-public class JdbcLancamentoEntity {
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.processador_encargos.domain.utils.Encargos;
+import org.example.processador_encargos.domain.utils.Processamentopg;
+import org.example.processador_encargos.domain.utils.SituacaoConta;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name = "lancamento_tb")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JpaLancamentoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private UUID contaId;
+    private BigDecimal valor;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Encargos encargos;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SituacaoConta statusconta;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Processamentopg processamentopg;
+    private LocalDateTime criadoEm;
+    private LocalDateTime processadoEm;
 }
