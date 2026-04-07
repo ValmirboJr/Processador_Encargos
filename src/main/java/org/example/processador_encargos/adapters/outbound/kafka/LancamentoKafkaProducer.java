92a72e5f922334a4254119558baba386e52911df
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-07 08:55:50 -0300

    feat: implementa producer de lançamentos com kafka

--- src/main/java/org/example/processador_encargos/adapters/outbound/kafka/LancamentoKafkaProducer.java
+++ src/main/java/org/example/processador_encargos/adapters/outbound/kafka/LancamentoKafkaProducer.java
@@ -1,4 +1,24 @@
 package org.example.processador_encargos.adapters.outbound.kafka;
 
+import lombok.RequiredArgsConstructor;
+import org.example.processador_encargos.domain.Lancamento;
+import org.example.processador_encargos.domain.event.RequestStatusEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class LancamentoKafkaProducer {
+
+    private final KafkaTemplate<String, RequestStatusEvent> kafkaTemplate;
+
+    public void publicarRequestStatus(Lancamento lancamento) {
+        RequestStatusEvent event = RequestStatusEvent.builder()
+                .contaId(lancamento.getContaId())
+                .lancamentoId(lancamento.getId())
+                .build();
+
+        kafkaTemplate.send("request-status-conta",
+                lancamento.getContaId().toString(), event);
+    }
 }
