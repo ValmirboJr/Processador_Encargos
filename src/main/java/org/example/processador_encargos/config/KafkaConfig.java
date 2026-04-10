4c4b7019e57ad5f3f0fd8d06c9738156471bdf0c
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-10 08:12:50 -0300

    refactor: simplifica configuracao do kafka removendo consumer factory customizado

--- src/main/java/org/example/processador_encargos/config/KafkaConfig.java
+++ src/main/java/org/example/processador_encargos/config/KafkaConfig.java
@@ -5,6 +5,8 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
@@ -31,5 +33,10 @@ public class KafkaConfig {
                 .partitions(32)
                 .replicas(1)
                 .build();
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
