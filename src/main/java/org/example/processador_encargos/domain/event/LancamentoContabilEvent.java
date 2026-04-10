46aa30ca6de8007af7d1d15846654a7e4dd14c3c
ValmirboJr <valmirborgesojr@gmail.com>
2026-04-10 08:13:04 -0300

    refactor: ajusta tipos dos campos do evento de lancamento contabil

--- src/main/java/org/example/processador_encargos/domain/event/LancamentoContabilEvent.java
+++ src/main/java/org/example/processador_encargos/domain/event/LancamentoContabilEvent.java
@@ -1,4 +1,21 @@
 package org.example.processador_encargos.domain.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.processador_encargos.domain.utils.Encargos;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LancamentoContabilEvent {
+    private UUID lancamentoId;
+    private long contaId;
+    private BigDecimal valor;
+    private Encargos encargos;
 }
